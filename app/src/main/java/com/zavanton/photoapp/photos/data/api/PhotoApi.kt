3f85c3e65527e12e1ddfe2d9c73bfa57c8837da3
zavanton123 <zavanton123@gmail.com>
2022-12-21 08:46:05 +0400

    downloadAfterId method added

--- app/src/main/java/com/zavanton/photoapp/photos/data/api/PhotoApi.kt
+++ app/src/main/java/com/zavanton/photoapp/photos/data/api/PhotoApi.kt
@@ -3,6 +3,7 @@ package com.zavanton.photoapp.photos.data.api
 import com.zavanton.photoapp.photos.data.api.models.PhotoApiModel
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.Query
 
 interface PhotoApi {
 
@@ -10,5 +11,11 @@ interface PhotoApi {
     suspend fun download(
         @Header("Authorization")
         authorization: String,
+    ): List<PhotoApiModel>
+
+    @GET("v1/items")
+    suspend fun downloadAfterId(
+        @Header("Authorization") authorization: String,
+        @Query("max_id") maxPhotoId: String,
     ): List<PhotoApiModel>
 }
